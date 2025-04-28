b3b8eb357d613b90bac5d09696a07f79242361de
zDeivide <deivideee12@hotmail.com>
2025-04-28 08:50:29 -0300

    Update app.js

--- Outlet Whisky/login/js/app.js
+++ Outlet Whisky/login/js/app.js
@@ -8,6 +8,7 @@ var log = false
 btn_cadastrar.addEventListener('click', salvar);
 
 function salvar(){
+    
     if(password.value == confirmation.value && user.value != '' && email.value != ''){
             localStorage.setItem("user", user.value);
             localStorage.setItem("password", password.value);
